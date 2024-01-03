6b9b9ff9de93b30db9534bde09d41f2fef8855de
Mostafa Essam <moustafa.essam@mimocodes.com>
2024-01-03 21:40:07 +0200

    add rabbitMQ config

--- app.js
+++ app.js
@@ -1,15 +1,35 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const { validateUser } = require('./middleware/validationMiddleware');
+const amqp = require('amqplib');
 
 const app = express();
 const port = 3000;
+const rabbitMQUrl = 'amqp://localhost';
+const queueName = 'userQueue';
 
+// Helper function to publish a message to RabbitMQ
+const publishToQueue = async (user) => {
+  try {
+    const connection = await amqp.connect(rabbitMQUrl);
+    const channel = await connection.createChannel();
+    await channel.assertQueue(queueName, { durable: false });
+    channel.sendToQueue(queueName, Buffer.from(JSON.stringify(user)));
+    console.log('User sent to RabbitMQ:', user);
+    await channel.close();
+    await connection.close();
+  } catch (error) {
+    console.error('Error publishing to RabbitMQ:', error);
+  }
+};
 app.use(bodyParser.json());
 
-app.post('/user', validateUser, (req, res) => {
+app.post('/user', validateUser, async (req, res) => {
   const user = req.body;
   console.log('User Recived! ', user);
+
+  await publishToQueue(user);
+
   res.status(200).json('Success');
 });
 
