7b10fea3289a307611cb13e1163681ffdc378767
Mostafa Essam <moustafa.essam@mimocodes.com>
2024-01-06 01:39:15 +0200

    change queue options

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ const publishToQueue = async (user) => {
   try {
     const connection = await amqp.connect(rabbitMQUrl);
     const channel = await connection.createChannel();
-    await channel.assertQueue(queueName, { durable: false });
+    await channel.assertQueue(queueName, { durable: true });
     channel.sendToQueue(queueName, Buffer.from(JSON.stringify(user)));
     console.log('User sent to RabbitMQ:', user);
     await channel.close();
